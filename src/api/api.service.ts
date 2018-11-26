0b97d0baa2a4c560fb2ecc063b0a8d74dce21355
kedar <kedarmail@gmail.com>
2018-11-26 12:32:06 -0800

    failover for results

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -4,11 +4,13 @@ import * as mongo from '../mongo.connection';
 @Injectable()
 export class ApiService {
     async getProposal(proposal_hash: string): Promise<any> {
-        return mongo.connection().then(con => con.actions.findOne({
+        const proposal = await mongo.connection().then(con => con.actions.findOne({
             "data.trace.act.account": "eparticlectr",
             "data.trace.act.name": "propose",
             "data.trace.act.data.proposed_article_hash": proposal_hash 
         }));
+        if (!proposal) return { "error": "Proposal not found" };
+        else return proposal;
     }
     async getVotes(proposal_hash: string): Promise<any> {
         return mongo.connection().then(con => con.actions.find({
@@ -18,11 +20,43 @@ export class ApiService {
         }).toArray());
     }
     async getResult(proposal_hash: string): Promise<any> {
-        return mongo.connection().then(con => con.actions.find({
+        const result = await mongo.connection().then(con => con.actions.findOne({
             "data.trace.act.account": "eparticlectr",
             "data.trace.act.name": "logpropres",
             "data.trace.act.data.proposal": proposal_hash 
-        }).toArray());
+        }));
+
+        if (result) return result;
+
+        const proposal = await this.getProposal(proposal_hash);
+        if (proposal.error) return { "error": "Proposal not found" };
+
+        const votes = await this.getVotes(proposal_hash);
+
+        const ret = {
+            proposal: proposal_hash,
+            approved: false,
+            finalized: false,
+            yes_votes: 50,
+            no_votes: 0
+        }
+
+        votes.forEach(function (vote) {
+            if (vote.data.trace.act.data.approve)
+                ret.yes_votes += vote.data.trace.act.data.amount;
+            else
+                ret.no_votes += vote.data.trace.act.data.amount;
+        });
+        if (ret.yes_votes > ret.no_votes) ret.approved = true;
+
+        const starttime = new Date(proposal.data.trace.block_time).getTime();
+        const now = new Date().getTime();
+        const SIX_HOURS = 6*3600*1000; // in milliseconds
+        if (now > starttime + SIX_HOURS)
+            ret.finalized = true;
+
+        return ret;
+
     }
     async getPlagiarism(proposal_hash: string): Promise<any> {
         return { "error": "Not implemented yet" }
