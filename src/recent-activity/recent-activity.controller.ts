54b1b8243d07dc2ca67ff0c507a8bfb67e077796
kedar <kedarmail@gmail.com>
2018-11-30 16:13:22 -0800

    add pagination to recent activity

--- src/recent-activity/recent-activity.controller.ts
+++ src/recent-activity/recent-activity.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiImplicitParam, ApiUseTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query, ParseIntPipe } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiImplicitParam, ApiImplicitQuery, ApiUseTags } from '@nestjs/swagger';
 import { RecentActivityService } from './recent-activity.service';
 
 @Controller('v1/recent-activity')
@@ -8,26 +8,37 @@ export class RecentActivityController {
   constructor(private readonly recentActivityService: RecentActivityService) {}
 
   @Get('all')
-  @ApiOperation({ title: "All recent on-chain activity" })
-  async getAll(): Promise<any> {
-    return await this.recentActivityService.getAll();
+  @ApiImplicitQuery({ name: "offset", description: "Number of records to skip", required: false, type: Number })
+  @ApiOperation({ title: "All recent on-chain activity", description: "Returns 100 most recent actions on the eparticlectr smart contract" })
+  async getAll(@Query('offset', new ParseIntPipe()) offset = 0): Promise<Array<any>> {
+    return await this.recentActivityService.getAll(offset);
   }
 
   @Get('results')
+  @ApiImplicitQuery({ name: "offset", description: "Number of records to skip", required: false, type: Number })
   @ApiOperation({ title: "Recent proposal results" })
-  async getResults(): Promise<any> {
-    return await this.recentActivityService.getResults();
+  async getResults(@Query('offset', new ParseIntPipe()) offset = 0): Promise<Array<any>> {
+    return await this.recentActivityService.getResults(offset);
   }
 
   @Get('votes')
+  @ApiImplicitQuery({ name: "offset", description: "Number of records to skip", required: false, type: Number })
   @ApiOperation({ title: "Recent on-chain votes" })
-  async getVotes(): Promise<any> {
-    return await this.recentActivityService.getVotes();
+  async getVotes(@Query('offset', new ParseIntPipe()) offset = 0): Promise<Array<any>> {
+    return await this.recentActivityService.getVotes(offset);
   }
 
   @Get('proposals')
+  @ApiImplicitQuery({ name: "offset", description: "Number of records to skip", required: false, type: Number })
   @ApiOperation({ title: "Recent proposals" })
-  async getProposals(): Promise<any> {
-    return await this.recentActivityService.getProposals();
+  async getProposals(@Query('offset') offset): Promise<Array<any>> {
+    return await this.recentActivityService.getProposals(offset);
+  }
+
+  @Get('wikis')
+  @ApiImplicitQuery({ name: "offset", description: "Number of records to skip", required: false, type: Number })
+  @ApiOperation({ title: "Recent accepted wikis" })
+  async getWikis(@Query('offset') offset): Promise<Array<any>> {
+    return await this.recentActivityService.getWikis(offset);
   }
 }
--- src/recent-activity/recent-activity.service.ts
+++ src/recent-activity/recent-activity.service.ts
@@ -3,14 +3,14 @@ import * as mongo from '../mongo.connection';
 
 @Injectable()
 export class RecentActivityService {
-    async getAll(): Promise<any> {
+    async getAll(offset: Number): Promise<Array<any>> {
         const docs = await mongo.connection().then(con => con.actions.find({
             "data.trace.act.account": "eparticlectr",
         }));
-        return docs.sort({ "data.block_num": -1 }).limit(100).toArray();
+        return docs.sort({ "data.block_num": -1 }).skip(offset).limit(100).toArray();
     }
 
-    async getResults(): Promise<any> {
+    async getResults(offset): Promise<Array<any>> {
         const results = await mongo.connection().then(con => con.actions.find({
             "data.trace.act.account": "eparticlectr",
             "data.trace.act.name": "logpropres",
@@ -18,7 +18,7 @@ export class RecentActivityService {
         return results.sort({"data.block_num": -1 }).limit(20).toArray();
     }
 
-    async getProposals(): Promise<any> {
+    async getProposals(offset): Promise<Array<any>> {
         const proposals = await mongo.connection().then(con => con.actions.find({
             "data.trace.act.account": "eparticlectr",
             "data.trace.act.name": "propose",
@@ -26,12 +26,21 @@ export class RecentActivityService {
         return proposals.sort({ "data.block_num": -1 }).limit(20).toArray();
     }
 
-    async getVotes(): Promise<any> {
+    async getVotes(offset): Promise<Array<any>> {
         const votes = await mongo.connection().then(con => con.actions.find({
             "data.trace.act.account": "eparticlectr",
             "data.trace.act.name": "votebyhash",
         }))
         return votes.sort({ "data.block_num": -1 }).limit(20).toArray();
+    }
+
+    async getWikis(offset): Promise<any> {
+        const results = await mongo.connection().then(con => con.actions.find({
+            "data.trace.act.account": "eparticlectr",
+            "data.trace.act.name": "logpropres",
+            "data.trace.act.data.approved": 1,
+        }));
+        return results.sort({"data.block_num": -1 }).limit(20).toArray();
     }
 
 }
