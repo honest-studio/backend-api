78682921d77561d441e957fd48336aaf38b2765a
kedar <kedarmail@gmail.com>
2019-01-21 11:04:52 -0800

    add text preview to recent activity proposals

--- src/recent-activity/recent-activity.service.ts
+++ src/recent-activity/recent-activity.service.ts
@@ -50,7 +50,8 @@ export class RecentActivityService {
             const article_info: Array<any> = await new Promise((resolve, reject) => {
                 this.mysql.pool().query(
                     `
-                    SELECT art.page_title, art.photo_url, art.photo_thumb_url, art.page_lang, cache.ipfs_hash
+                    SELECT art.page_title, art.photo_url, art.photo_thumb_url, art.page_lang, 
+                        cache.ipfs_hash, art.blurb_snippet AS text_preview
                     FROM enterlink_articletable AS art 
                     JOIN enterlink_hashcache AS cache
                     ON cache.articletable_id=art.id
