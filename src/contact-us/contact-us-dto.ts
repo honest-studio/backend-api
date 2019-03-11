ac924afca918f1eb66e597ea600b635239d9d76f
travis <travis@everipedia.com>
2019-03-11 14:52:46 -0700

    Contact Us

--- src/contact-us/contact-us-dto.ts
+++ src/contact-us/contact-us-dto.ts
@@ -19,6 +19,45 @@ export class ContactUSDto {
     upload_type: string;
 }
 
+
+// @ApiImplicitParam({
+//     name: 'contact_date',
+//     required: true,
+//     description: 'The date'
+// })
+// @ApiImplicitParam({
+//     name: 'contact_text',
+//     required: true,
+//     description: 'The text of the contact us form'
+// })
+// @ApiImplicitParam({
+//     name: 'contact_email',
+//     required: true,
+//     description: 'The email of the submitter'
+// })
+// @ApiImplicitParam({
+//     name: 'contact_subject',
+//     required: true,
+//     description: 'Subject'
+// })
+// @ApiImplicitParam({
+//     name: 'contact_type',
+//     required: true,
+//     description: 'The type of contact'
+// })
+// @ApiImplicitParam({
+//     name: 'contact_ip',
+//     required: true,
+//     description: 'IP address of the contact'
+// })
+// @ApiImplicitParam({
+//     name: 'contact_useragent',
+//     required: true,
+//     description: 'The user agent'
+// })
+
+
+
 // Mimetype interface
 export interface MimePack {
     ext: string;
