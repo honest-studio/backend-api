a28260387525636303d8d69f056a6765a15a4a33
travis <travis@everipedia.com>
2019-07-10 15:08:41 -0700

    slug fixes

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -17,7 +17,7 @@ export class HistoryService {
         // idk why, I just work with what I have ¯\_(ツ)_/¯
         // so we search for both regular and encoded slugs
         const mysql_slug = this.mysql.cleanSlugForMysql(slug);
-        const proposal_id_docs = await this.mongo
+        let proposal_id_docs = await this.mongo
             .connection()
             .actions.find(
                 {
@@ -25,7 +25,7 @@ export class HistoryService {
                     'trace.act.name': 'logpropinfo',
                     $or: [
                         { 'trace.act.data.slug': slug },
-                        { 'trace.act.data.slug': mysql_slug }
+                        { 'trace.act.data.slug': mysql_slug },
                     ],
                     'trace.act.data.lang_code': lang_code
                 },
@@ -33,14 +33,33 @@ export class HistoryService {
             )
             .sort({ 'trace.act.data.proposal_id': -1 })
             .toArray();
+
+        // If the proposal isn't found yet, try more slugs 
+        if (!proposal_id_docs.length){
+            proposal_id_docs = await this.mongo
+            .connection()
+            .actions.find(
+                {
+                    'trace.act.account': 'eparticlectr',
+                    'trace.act.name': 'logpropinfo',
+                    $or: [
+                        { 'trace.act.data.slug': encodeURIComponent(slug) },
+                        { 'trace.act.data.slug': encodeURIComponent(mysql_slug) },
+                    ],
+                    'trace.act.data.lang_code': lang_code
+                },
+                { projection: { 'trace.act.data.proposal_id': 1 } }
+            )
+            .sort({ 'trace.act.data.proposal_id': -1 })
+            .toArray();
+        }
+
         const proposal_ids = proposal_id_docs
             .map((doc) => doc.trace.act.data.proposal_id)
             .filter((v, i, arr) => arr.indexOf(v) === i) // get unique values
             .map(Number);
 
         const proposals = await this.proposalService.getProposals(proposal_ids, options);
-
-        console.log(slug);
 
         return proposals;
     }
