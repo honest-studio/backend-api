85a97435170e591f40934b447831cf735ee5aed3
travis <travis@everipedia.com>
2019-07-30 13:13:17 -0700

    Patcher code part 3

--- scripts/batch/batch-commands.py
+++ scripts/batch/batch-commands.py
@@ -7,17 +7,17 @@ import os
 
 commands = [
 'export PYTHONIOENCODING=utf-8',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 1000 -e 100000 >> scripts/batch/log/batch_00.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 100000 -e 200000 >> scripts/batch/log/batch_01.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 200000 -e 300000 >> scripts/batch/log/batch_02.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 300000 -e 400000 >> scripts/batch/log/batch_03.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 400000 -e 500000 >> scripts/batch/log/batch_04.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 500000 -e 600000 >> scripts/batch/log/batch_05.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 600000 -e 700000 >> scripts/batch/log/batch_06.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 700000 -e 800000 >> scripts/batch/log/batch_07.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 800000 -e 900000 >> scripts/batch/log/batch_08.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 900000 -e 1000000 >> scripts/batch/log/batch_09.log 2>&1',
-'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 1000000 -e 1100000 >> scripts/batch/log/batch_10.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 1100000 -e 4000000 >> scripts/batch/log/batch_00.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 4000000 -e 8000000 >> scripts/batch/log/batch_01.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 8000000 -e 12000000 >> scripts/batch/log/batch_02.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 12000000 -e 16000000 >> scripts/batch/log/batch_03.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 16000000 -e 20000000 >> scripts/batch/log/batch_04.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 20000000 -e 24000000 >> scripts/batch/log/batch_05.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 24000000 -e 28000000 >> scripts/batch/log/batch_06.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 28000000 -e 32000000 >> scripts/batch/log/batch_07.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 32000000 -e 36000000 >> scripts/batch/log/batch_08.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 36000000 -e 40000000 >> scripts/batch/log/batch_09.log 2>&1',
+'node dist/scripts/Non-Lambda/MergeMediaAndPatchInfoboxes.js -s 40000000 -e 44000000 >> scripts/batch/log/batch_10.log 2>&1',
 ]
 # run in parallel
 processes = [Popen(cmd, shell=True) for cmd in commands]
