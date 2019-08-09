9e795225627f05023af5a690b9f45eb83a16b405
travis <travis@everipedia.com>
2019-08-08 17:25:31 -0700

    Typescript fix

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -5,7 +5,7 @@ import { ApiOperation, ApiUseTags } from '@nestjs/swagger';
 @Controller('v2/status')
 @ApiUseTags('Status')
 export class StatusController {
-    constructor(private readonly ipfs: IpfsService) {}
+    constructor(private readonly ipfsLocal: IpfsService) {}
 
     @Get('ipfs')
     @ApiOperation({ 
@@ -13,7 +13,7 @@ export class StatusController {
         description: 'Returns 200 if working, 500 if not'
     })
     async ipfs(): Promise<boolean> {
-        return this.ipfs.test();
+        return this.ipfsLocal.test();
     }
 
 }
