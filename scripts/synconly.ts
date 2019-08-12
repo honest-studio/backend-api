9e9f338de2eebbe1142ee8a773c14eebc2a77132
kedar <kedarmail@gmail.com>
2019-08-11 21:36:52 -0700

    redis pub sub

--- scripts/synconly.ts
+++ scripts/synconly.ts
@@ -1,3 +1,7 @@
+// In production , there are multiple instances running of the repo
+// To prevent the multiple nodes from duplicating sync work and causing DB lockups
+// this script should be used in a separate process
+
 import fetch from 'node-fetch';
 import WebSocket from 'ws';
 import { Collection, Db, MongoClient } from 'mongodb';
@@ -96,20 +100,21 @@ async function start() {
                 const block_num = msg.data.block_num;
                 const account = msg.data.trace.act.account;
                 const name = msg.data.trace.act.name;
-                console.log(`EOS-SYNC-SERVICE: Saved ${account}:${name} @ block ${block_num} to Mongo`);
+                redis.publish("eos_actions", JSON.stringify([msg.data]));
+                console.log(`MONGO: Saved ${account}:${name} @ block ${block_num}`);
             })
             .catch((err) => {
                 if (err.code == 11000) {
-                    console.log(`EOS-SYNC-SERVICE: Ignoring duplicate action. This is expected behavior during server restarts or cluster deployments`);
+                    console.log(`MONGO: Ignoring duplicate action. This is expected behavior during server restarts or cluster deployments`);
                 }
                 else {
-                    console.log('EOS-SYNC-SERVICE: Error inserting action ', msg, ' \n Error message on insert: ', err);
+                    console.log('MONGO: Error inserting action ', msg, ' \n Error message on insert: ', err);
                     throw err;
                 }
             });
     });
     dfuse.on('error', (e) => {
-        console.log('EOS-SYNC-SERVICE: Dfuse error: ', e);
+        console.log('DFUSE: ERROR: ', e);
     });
 }
 
@@ -132,83 +137,7 @@ function restartIfFailing() {
     }
 }
 
-async function redisUpdate () {
 
-    console.log(`EOS-SYNC-SERVICE: Building redis cache`);
-    await redis.flushdb();
-    const mongo_actions = await mongo_actions_promise;
-
-    while (true) {
-        const last_processed = await redis.get('mongo:last_processed');
-        let query; 
-        let block_num;
-        if (!last_processed) {
-            query = {}
-            block_num = 0;
-        }
-        else {
-            block_num = JSON.parse(last_processed).block_num;
-            query = { block_num: { $gt: block_num }};
-        }
-        const actions = await mongo_actions.find(query).limit(50000).toArray();
-        console.log(`EOS-SYNC-SERVICE: Redis: Processing ${actions.length} actions from block ${block_num}`);
-
-        const pipeline = redis.pipeline();
-        for (let action of actions) {
-            if (action.trace.act.name == "vote" || action.trace.act.name == "votebyhash") {
-                const proposal_id = action.trace.act.data.proposal_id;
-                pipeline.sadd(`proposal:${proposal_id}:votes`, JSON.stringify(action));
-                const user = action.trace.act.data.voter;
-                pipeline.incr(`user:${user}:num_votes`);
-            }
-            else if (action.trace.act.name == "logpropinfo") {
-                const proposal_id = action.trace.act.data.proposal_id;
-                pipeline.set(`proposal:${proposal_id}:info`, JSON.stringify(action));
-            }
-            else if (action.trace.act.name == "logpropres") {
-                const proposal_id = action.trace.act.data.proposal_id;
-                pipeline.set(`proposal:${proposal_id}:result`, JSON.stringify(action));
-            }
-            else if (action.trace.act.name == "propose" || action.trace.act.name == "propose2") {
-                const user = action.trace.act.data.proposer;
-                pipeline.incr(`user:${user}:num_edits`);
-            }
-            else if (action.trace.act.name == "issue") {
-                const user = action.trace.act.data.to;
-                const amount = action.trace.act.data.quantity.split(' ')[0];
-                // All-time leaderboard
-                pipeline.zincrby("editor-leaderboard:all-time:rewards", amount, user);
-            }
-            else if (action.trace.act.name == "transfer") {
-                if (action.trace.act.data.to == "eparticlectr") {
-                    const user = action.trace.act.data.from;
-                    const amount = action.trace.act.data.quantity.split(' ')[0];
-                    pipeline.rpush(`user:${user}:stakes`, JSON.stringify(action));
-                    pipeline.incrbyfloat(`user:${user}:sum_stakes`, amount);
-                }
-                else if (action.trace.act.data.from == "eparticlectr") {
-                    const user = action.trace.act.data.to;
-                    const amount = action.trace.act.data.quantity.split(' ')[0];
-                    pipeline.rpush(`user:${user}:refunds`, JSON.stringify(action));
-                    pipeline.incrbyfloat(`user:${user}:sum_refunds`, amount);
-                }
-            }
-        }
-        if (actions.length == 0) break;
-        else {
-            pipeline.exec();
-            const last_processed = {
-                _id: actions[actions.length - 1]._id,
-                block_num: actions[actions.length - 1].block_num
-            };
-            await redis.set('mongo:last_processed', JSON.stringify(last_processed));
-        }
-    } 
-    console.log(`EOS-SYNC-SERVICE: Done building redis cache`);
-
-}
-
-redisUpdate();
 start();
 setInterval(() => restartIfFailing.apply(this), 15 * 1000);
 
--- src/feature-modules/database/redis-service.ts
+++ src/feature-modules/database/redis-service.ts
@@ -8,10 +8,83 @@ export class RedisService {
 
     constructor() { 
         this.redis = new Redis();
+        this.redis.subscribe("eos_actions");
+        this.redis.on("message", function(channel, message) {
+            if (channel == "eos_actions")
+                this.process_actions(JSON.parse(message));
+        });
+
     }
 
     connection() {
         return this.redis;
     }
 
+    async process_actions (actions) {
+
+        while (true) {
+            const last_processed = await this.redis.get('mongo:last_processed');
+            let last_global_sequence = 0;
+            if (last_processed) {
+                last_global_sequence = JSON.parse(last_processed).global_sequence;
+            }
+
+            const pipeline = this.redis.pipeline();
+            for (let action of actions) {
+                if (action.trace.receipt.global_sequence <= last_global_sequence) {
+                    console.log(`Skipping action ${action.trace.receipt.global_sequence}: Already processed`);
+                    continue;
+                }
+                if (action.trace.act.name == "vote" || action.trace.act.name == "votebyhash") {
+                    const proposal_id = action.trace.act.data.proposal_id;
+                    pipeline.sadd(`proposal:${proposal_id}:votes`, JSON.stringify(action));
+                    const user = action.trace.act.data.voter;
+                    pipeline.incr(`user:${user}:num_votes`);
+                }
+                else if (action.trace.act.name == "logpropinfo") {
+                    const proposal_id = action.trace.act.data.proposal_id;
+                    pipeline.set(`proposal:${proposal_id}:info`, JSON.stringify(action));
+                }
+                else if (action.trace.act.name == "logpropres") {
+                    const proposal_id = action.trace.act.data.proposal_id;
+                    pipeline.set(`proposal:${proposal_id}:result`, JSON.stringify(action));
+                }
+                else if (action.trace.act.name == "propose" || action.trace.act.name == "propose2") {
+                    const user = action.trace.act.data.proposer;
+                    pipeline.incr(`user:${user}:num_edits`);
+                }
+                else if (action.trace.act.name == "issue") {
+                    const user = action.trace.act.data.to;
+                    const amount = action.trace.act.data.quantity.split(' ')[0];
+                    // All-time leaderboard
+                    pipeline.zincrby("editor-leaderboard:all-time:rewards", amount, user);
+                }
+                else if (action.trace.act.name == "transfer") {
+                    if (action.trace.act.data.to == "eparticlectr") {
+                        const user = action.trace.act.data.from;
+                        const amount = action.trace.act.data.quantity.split(' ')[0];
+                        pipeline.rpush(`user:${user}:stakes`, JSON.stringify(action));
+                        pipeline.incrbyfloat(`user:${user}:sum_stakes`, amount);
+                    }
+                    else if (action.trace.act.data.from == "eparticlectr") {
+                        const user = action.trace.act.data.to;
+                        const amount = action.trace.act.data.quantity.split(' ')[0];
+                        pipeline.rpush(`user:${user}:refunds`, JSON.stringify(action));
+                        pipeline.incrbyfloat(`user:${user}:sum_refunds`, amount);
+                    }
+                }
+            }
+            if (actions.length == 0) break;
+            else {
+                pipeline.exec();
+                const last_processed = {
+                    global_sequence: Number(actions[actions.length - 1].trace.receipt.global_sequence),
+                    block_num: actions[actions.length - 1].block_num
+                };
+                await this.redis.set('mongo:last_processed', JSON.stringify(last_processed));
+            }
+        } 
+        console.log(`REDIS: Done building cache`);
+
+    }
 }
