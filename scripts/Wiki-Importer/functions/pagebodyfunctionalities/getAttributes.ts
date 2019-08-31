2f35bde8631b2639d322f2fc94544dfa57334d86
travis <travis@everipedia.com>
2019-08-31 13:51:05 -0700

    Wiki-Importer integration Typescript 12

--- scripts/Wiki-Importer/functions/pagebodyfunctionalities/getAttributes.ts
+++ scripts/Wiki-Importer/functions/pagebodyfunctionalities/getAttributes.ts
@@ -1,23 +1,31 @@
 // Travis Moore and Kedar Iyer's code to clean attributes to fit front-end requirements
-import { convert } from 'react-attr-converter'; //convert attributes to React format 
+import { convert } from 'react-attr-converter';
 
+// Convert attributes to React format 
 export const cleanAttrs = (attributes) => {
-	if (attributes === undefined || attributes === null) { //prevent error for empty inputs
+    // Prevent error for empty inputs
+	if (attributes === undefined || attributes === null) {
 		return {}
 	}
 	const cleanedAttrs = {};
     const keys = Object.keys(attributes);
+
+    // Look for the non-react CSS name and convert it to the React format
     for (const key of keys) {
         if (attributes[key] && attributes[key] != '') {
         	cleanedAttrs[convert(key)] = attributes[key];
         }
     }
+
+    // If a 'style' attribute is present, combine all of the styles into one style string 
     if (cleanedAttrs['style']){
         cleanedAttrs['style'] = parseStyles(cleanedAttrs['style']);
     } 
     return cleanedAttrs;
 }
 
+// Used if a 'style' attribute is present
+// Combine all of the styles into one style string 
 export const parseStyles = (styles) => {
     return styles
     .split(';')
