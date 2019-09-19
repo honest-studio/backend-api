f85ad2ee0501a16483b0a0cf24b0a4b24ac1777b
kedar <kedarmail@gmail.com>
2019-09-19 15:23:29 -0700

    Revert "only add back wiki caching"
    This reverts commit 958667e86ac0c27ec2e116c90e7410c0e572a9b6.

--- src/wiki/wiki.service.ts
+++ src/wiki/wiki.service.ts
@@ -174,7 +174,10 @@ export class WikiService {
             throw new HttpException(`Wiki ${lang_code}/${slug} is marked as removed`, HttpStatus.GONE);
 
         // Try and get cached wiki
-        if (current_hash) {
+        console.log(colors.red("NEED TO REMOVE THE && false"));
+        console.log(colors.red("NEED TO REMOVE THE && false"));
+        console.log(colors.red("NEED TO REMOVE THE && false"));
+        if (current_hash && false) {
             const cache_wiki = await this.redis.connection().get(`wiki:${current_hash}`);
             if (cache_wiki) return JSON.parse(cache_wiki);
         }
