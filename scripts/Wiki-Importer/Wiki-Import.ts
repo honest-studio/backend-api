02f7e0a1be2f521b9203977059ca8e573d6c19f6
travis <travis@everipedia.com>
2019-10-06 12:54:15 -0700

    Article Merge Fix

--- scripts/Wiki-Importer/Wiki-Import.ts
+++ scripts/Wiki-Importer/Wiki-Import.ts
@@ -40,9 +40,9 @@ commander
   .parse(process.argv);
 
 const BATCH_SIZE = 250;
-// const LASTMOD_CUTOFF_TIME = '2019-09-18 02:35:19';
+const LASTMOD_CUTOFF_TIME = '2019-09-18 02:35:19';
 // const BATCH_SIZE = 1;
-const LASTMOD_CUTOFF_TIME = '2099-09-14 00:00:00';
+// const LASTMOD_CUTOFF_TIME = '2099-09-14 00:00:00';
 const PAGE_NOTE = '|EN_WIKI_IMPORT|';
 
 export const logYlw = (inputString: string) => {
