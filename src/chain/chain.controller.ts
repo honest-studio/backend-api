c9d47bb84ad023efd0c771e783ebbd8d404bf77a
kedar <kedarmail@gmail.com>
2019-11-04 18:26:17 -0800

    pay for users' CPU

--- src/chain/chain.controller.ts
+++ src/chain/chain.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiUseTags } from '@nestjs/swagger';
 import { ChainService } from './chain.service';
 
-@Controller('v2/chain')
+@Controller('v1/chain')
 @ApiUseTags('Chain')
 export class ChainController {
     constructor(private readonly chainService: ChainService) {}
@@ -23,6 +23,7 @@ export class ChainController {
         title: 'Guaranteed transaction execution',
         description: `
             An improved version of the EOS HTTP API push_transaction endpoint that offers guaranteed transaction execution.
+            The 'Content-Type: application/json' header must be set to use this endpoint.
             This is a slow endpoint that does not return until the transaction has been included in a block.
             Details for using the endpoint can be found at https://developers.eos.io/eosio-nodeos/reference#push_transaction.`
     })
@@ -35,6 +36,7 @@ export class ChainController {
         title: 'Get table rows',
         description: `
             Returns an object containing rows from the specified table.
+            The 'Content-Type: application/json' header must be set to use this endpoint.
             Details for using the endpoint can be found at https://developers.eos.io/eosio-nodeos/reference#get_table_rows.`
     })
     async getTableRows(@Body() body): Promise<any> {
@@ -46,6 +48,7 @@ export class ChainController {
         title: 'EOS Chain API',
         description: `
             A drop-in replacement for the EOS HTTP Chain API.
+            The 'Content-Type: application/json' header must be set to use this endpoint.
             Details for using the endpoint can be found at https://developers.eos.io/eosio-nodeos/reference`
     })
     async forward(@Param('eos_api_endpoint') eos_api_endpoint, @Body() body): Promise<any> {
