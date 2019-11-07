b07c4b3f81ce10169d9d602f25f58b16e7c197c3
kedar <kedarmail@gmail.com>
2019-11-07 12:40:27 -0800

    security fix for CPU payments

--- src/chain/chain.service.ts
+++ src/chain/chain.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import * as fetch from 'node-fetch';
 import { ConfigService } from '../common';
-import { Api, JsonRpc, RpcError } from 'eosjs';
+const { Api, JsonRpc, RpcError } = require('eosjs');
 import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
 import ecc from 'eosjs-ecc';
 
@@ -34,12 +34,37 @@ export class ChainService {
     // Everipedia will pay for CPU that goes through here, so it is limited to 
     // Everipedia actions only
     async sign(transaction): Promise<any> {
-        console.log(transaction);
+
         const privkey = this.config.get("PAY_CPU_PRIVKEY");
         const pubkey = this.config.get("PAY_CPU_PUBKEY");
-        const signer = new JsSignatureProvider([privkey]);
+        const signatureProvider = new JsSignatureProvider([privkey]);
+        const rpc = new JsonRpc('http://127.0.0.1:8888', { fetch });
+        const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+        // Make sure actions are only directed at Everipedia contracts
+        const WHITELISTED_CONTRACTS = ["everipediaiq", "eparticlectr", "epsovreignid"];
+        const transaction_buffer = Buffer.from(transaction.serializedTransaction.data);
+        const tx = api.deserializeTransaction(transaction_buffer);
+        for (let action of tx.actions) {
+            if (!WHITELISTED_CONTRACTS.includes(action.account)) {
+                const message = `FREELOADER ALERT: CPU payments not supported for ${action.account}`;
+                console.warn("============= FREELOADER ALERT ===========");
+                console.warn(message);
+                console.warn("============= FREELOADER ALERT ===========");
+                throw new BadRequestException(message);
+            }
+
+            // 70e89bf4a254ef56 is the hex eos name encoding for evrpdcronjob
+            if (action.data.toLowerCase().includes("70e89bf4a254ef56")) {
+                const message = `HACKER ALERT: evrpdcronjob cannot be involved in the action.`;
+                console.warn("============= HACKER ALERT ===========");
+                console.warn(message);
+                console.warn("============= HACKER ALERT ===========");
+                throw new BadRequestException(message);
+            }
+        }
         const signBuf = Buffer.concat([
-            Buffer.from(transaction.chainId, 'hex'), Buffer.from(transaction.serializedTransaction.data), new Buffer(new Uint8Array(32)),
+            Buffer.from(transaction.chainId, 'hex'), transaction_buffer, Buffer.from(new Uint8Array(32)),
         ]);
         const sig = ecc.Signature.sign(signBuf, privkey).toString();
 
