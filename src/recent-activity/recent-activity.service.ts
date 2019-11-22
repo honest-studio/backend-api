6414be2f0986c1154b33654634bad1a807c54042
kedar <kedarmail@gmail.com>
2019-11-21 17:51:15 -0800

    langs and voter can be used together in recent activity

--- src/recent-activity/recent-activity.service.ts
+++ src/recent-activity/recent-activity.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { Injectable, Inject, forwardRef, BadRequestException } from '@nestjs/common';
 import { MongoDbService, MysqlService, RedisService } from '../feature-modules/database';
 import { EosAction } from '../feature-modules/database/mongodb-schema';
 import { PreviewService } from '../preview/preview.service';
@@ -58,6 +58,9 @@ export class RecentActivityService {
     }
 
     async getProposals(query): Promise<Array<Proposal>> {
+        if (query.voter && query.account_name)
+            throw new BadRequestException("voter and account_name cannot be used together");
+
         let find_query;
         let sort_direction;
         const now = (Date.now() / 1000) | 0;
@@ -89,12 +92,12 @@ export class RecentActivityService {
                 .toArray();
             find_query['trace.act.data.proposal_id'] = { $lt: last_finalized[0].trace.act.data.proposal_id };
         }
-        if (query.langs) {
+        if (query.langs && !query.voter) {
             find_query['trace.act.data.lang_code'] = { $in: query.langs.split(',') };
         }
-        const proposal_id_docs = await this.mongo
+        let proposal_id_docs = await this.mongo
             .connection()
-            .actions.find(find_query, { projection: { 'trace.act.data.proposal_id': 1 } })
+            .actions.find(find_query, { projection: { 'trace.act.data': 1 } })
             .sort({ 'trace.act.data.proposal_id': sort_direction })
             .skip(query.offset)
             .limit(query.limit)
